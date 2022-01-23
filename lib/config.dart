421d55281fad8686539db68400867ddc6d08411f
Thakshara <thaksharadhananjaya@gamil.com>
2022-01-24 01:16:01 +0530

    flutter test

--- lib/config.dart
+++ lib/config.dart
@@ -4,4 +4,4 @@ const kPrimaryColor = Colors.blue;
 const kBackgroundColor = Colors.white;
 const kPaddingHorizontal = 16.0;
 const String accessKey = "z\$#PYc3E/:33VTiVB]T3p!c3E/Kcx!";
-const String url = "http://3.110.207.205/test"; //end point (use free aws ec2 ec2 instance)
+const String url = "http://3.110.207.205/test"; //end point (use free aws ec2 instance)
